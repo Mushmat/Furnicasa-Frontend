0ba2452930a0b82f15d2e24f44d06921be2bd35e
Mushmat <edu.chirayu2005@gmail.com>
2025-03-17 13:11:52 +0530

    Cart fix newv1

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -11,7 +11,7 @@ const Cart = () => {
   const fetchCart = async () => {
     try {
       const token = localStorage.getItem('token');
-      const res = await axios.get(BACKEND_URL, {
+      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/api/cart`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
