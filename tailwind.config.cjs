9527201e91f736e9132dc8674a7c47f7071a96a6
Mushmat <edu.chirayu2005@gmail.com>
2025-05-25 15:08:38 +0530

    changed font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'media',
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+  darkMode: 'media',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // override the default "sans" stack with your Google font first
+        sans: ['"WDXL Lubrifont TC"', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+};
