39d2438ea02624e86f61bdd716159735192c5401
Mushmat <edu.chirayu2005@gmail.com>
2025-05-27 12:19:21 +0530

    shop now background fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -91,9 +91,12 @@ export default function Home() {
                     <br />
                     Modern &amp; Exclusive Furniture
                   </h2>
-                  <Link to="/products" className="btn bg-white/80 hover:bg-white">
-                    SHOP NOW
-                  </Link>
+                  <Link
+                        to="/products"
+                        className="btn bg-white text-gray-900 hover:bg-gray-100"
+                      >
+                        SHOP NOW
+                      </Link>
                 </div>
               </div>
             </div>
